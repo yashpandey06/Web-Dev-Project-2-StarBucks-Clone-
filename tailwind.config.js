55eb41d80f48e31a089ccabadb7851d4efddfedd
Yash Pandey <pandeyyash7052@gmail.com>
2022-10-24 00:13:28 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontSize:{
+sm:["14px"]
+      },
+      fontFamily :{
+Notosans :['Noto Sans', "sans-serif"],
+      },
+      colors:{
+        stargreen:"#D4E9E2",
+        darkgreen:"#1E3932",
+      },
+    },
+  },
+  plugins: [],
+}
